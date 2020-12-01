2cd5c083af965e963c048f4fe35509380daec54e
Toulouse Jérôme <monsieur.toulouse@gmail.com>
2020-12-01 21:52:40 +0100

    solve 1_2

--- 2020/day_one/expense_report/lib/expense_report.ex
+++ 2020/day_one/expense_report/lib/expense_report.ex
@@ -8,14 +8,41 @@ defmodule ExpenseReport do
     a * b
   end
 
+  def solve_1_2(inputs) do
+    [a, b, c] =
+      inputs
+      |> get_inputs()
+      |> iterate()
+
+    a * b * c
+  end
+
+  def iterate([lim | tail]) do
+    case find_matching_nums_to(tail, 2020 - lim) do
+      [x, y] ->
+        [x, y, lim]
+
+      [] ->
+        iterate(tail ++ [lim])
+    end
+  end
+
   # PRIVATE
+
+  def sum_up_to(x, y, lim) do
+    x + y == lim
+  end
 
   defp sum_up(x, y) do
     x + y == 2020
   end
 
+  def find_matching_nums([]) do
+    []
+  end
+
   def find_matching_nums([x | nums]) do
-    case filter(x, nums) do
+    case Enum.filter(nums, fn n -> sum_up(x, n) end) do
       [y] ->
         [x, y]
 
@@ -24,9 +51,18 @@ defmodule ExpenseReport do
     end
   end
 
-  def filter(x, nums) do
-    nums
-    |> Enum.filter(fn n -> sum_up(x, n) end)
+  def find_matching_nums_to([], _) do
+    []
+  end
+
+  def find_matching_nums_to([x | nums], limit) do
+    case Enum.filter(nums, fn n -> sum_up_to(x, n, limit) end) do
+      [y] ->
+        [x, y]
+
+      [] ->
+        find_matching_nums_to(nums, limit)
+    end
   end
 
   defp get_inputs(filename) do
