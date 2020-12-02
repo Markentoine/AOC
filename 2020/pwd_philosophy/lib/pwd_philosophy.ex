b28d5beef946f2499e11f1d9beaaa71aa5b2db24
Toulouse Jérôme <monsieur.toulouse@gmail.com>
2020-12-02 08:58:02 +0100

    begins day two

---
+++ 2020/pwd_philosophy/lib/pwd_philosophy.ex
@@ -0,0 +1,18 @@
+defmodule PwdPhilosophy do
+  @moduledoc """
+  Documentation for `PwdPhilosophy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PwdPhilosophy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ 2020/pwd_philosophy/test/pwd_philosophy_test.exs
@@ -0,0 +1,8 @@
+defmodule PwdPhilosophyTest do
+  use ExUnit.Case
+  doctest PwdPhilosophy
+
+  test "greets the world" do
+    assert PwdPhilosophy.hello() == :world
+  end
+end
