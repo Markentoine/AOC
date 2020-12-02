e09f4982b13384a30ac989c3d586e2ea69da5296
Toulouse Jérôme <monsieur.toulouse@gmail.com>
2020-12-02 16:36:52 +0100

    solution day 2 2

--- 2020/day_two/pwd_philosophy/lib/pwd_philosophy.ex
+++ 2020/day_two/pwd_philosophy/lib/pwd_philosophy.ex
@@ -1,13 +1,25 @@
 defmodule PwdPhilosophy do
-  def solve_2_1 do
+  def solve_2_1, do: solve(&match_policy/1)
+
+  def solve_2_2, do: solve(&match_real_policy/1)
+
+  # PRIVATE
+  defp solve(fun) do
     "pwd_db.txt"
     |> get_inputs()
     |> serialize_pwd_infos()
-    |> Enum.filter(&match_policy/1)
+    |> Enum.filter(fun)
     |> Enum.count()
   end
 
-  # PRIVATE
+  defp match_real_policy(%{letter: letter, range: range, pwd: pwd}) do
+    [pos1, pos2] = range(range)
+    list_chars = pwd |> String.split("", trim: true)
+    char1 = Enum.at(list_chars, pos1 - 1)
+    char2 = Enum.at(list_chars, pos2 - 1)
+    !(char1 == letter and char2 == letter) and (char1 == letter or char2 == letter)
+  end
+
   defp serialize_pwd_infos(rows) do
     rows
     |> Enum.map(&pwd_infos/1)
