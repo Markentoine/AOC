86ac2c8426a02a8501d87dbf87d82241101a1178
Toulouse Jérôme <monsieur.toulouse@gmail.com>
2020-12-04 19:22:51 +0100

    solutions  2015_1_1_and_2.

--- 2015/day_one/lib/day_one.ex
+++ 2015/day_one/lib/day_one.ex
@@ -1,22 +1,52 @@
 defmodule DayOne do
-  @moduledoc """
-  Documentation for DayOne.
-  """
+  def solve_1_1 do
+    "directions.txt"
+    |> get_inputs()
+    |> String.split("", trim: true)
+    |> Enum.reduce(0, fn i, floor ->
+      cond do
+        i == "(" ->
+          floor + 1
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> DayOne.hello()
-      :world
-
-  """
-  def hello do
-    :world
+        i == ")" ->
+          floor - 1
+      end
+    end)
   end
 
-  def what_floor(instructions) do
+  def solve_1_2 do
+    "directions.txt"
+    |> get_inputs()
+    |> String.split("", trim: true)
+    |> basement(0, 0)
+  end
 
+  def basement([], _index, floor), do: floor
+
+  def basement(_directions, index, -1), do: index
+
+  def basement(directions, index, floor) do
+    [dir | rest] = directions
+
+    cond do
+      dir == "(" ->
+        basement(rest, index + 1, floor + 1)
+
+      dir == ")" ->
+        basement(rest, index + 1, floor - 1)
+    end
+  end
+
+  # PRIVATE
+  defp get_inputs(filename) do
+    filename
+    |> get_path()
+    |> File.read!()
+    |> String.trim()
+  end
+
+  defp get_path(filename) do
+    filename
+    |> Path.expand(__DIR__)
   end
 end
